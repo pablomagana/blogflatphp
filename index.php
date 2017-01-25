a1c4f2c5b0576cf62e8c160cf066039433194f07
Pablo Magaña Ubeda <pablomagub@gmail.com>
2017-01-25 18:15:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo "<h1>Bienvenido al blog !</h1>";
 echo "User: develop pablo.. cerrar sesion.";
 echo "<br/>";
 echo "zona admin super<br/>";
-echo "detectada intrusión";
+echo "detectada intrusión!!";
 if(isset($_GET['id'])){
   //lista.php modificada
   include ('controller/detalle.php');
